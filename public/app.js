3d9914691a532be98213e9aaba61b782d1e73835
Lee Lawrence <leerlawrence@gmail.com>
2016-09-27 20:21:40 +0100

    maps

---
+++ public/app.js
@@ -0,0 +1,13 @@
+
+var initialize= function(){
+var container = document.getElementById('map');
+var center = { lat: 55.94716, lng: -3.20198};
+//{ lat: 55.94716, lng: -3.20198}
+var map = new Map(container, center, 10);
+map.addMarker(center);
+map.addClickEvent();
+}
+
+
+
+window.onload = initialize;
